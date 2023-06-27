ae410844277977ea49fefe428887d6298200469e
Juebele <eronuebele@gmail.com>
2023-06-27 14:28:50 -0400

    removed some random crap

--- airplane-site/src/App.js
+++ airplane-site/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Header from './components/Header'
+import Main from './components/Main'
+
 
 function App() {
   return (
     <div class="body">
+      <Header/>
       <Main/>
     </div>
   );
--- airplane-site/src/components/Main.js
+++ airplane-site/src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Main(){
     return (
-        <p>This is a test</p>
+        <p>This is where the main content will go</p>
     )
 }
 
